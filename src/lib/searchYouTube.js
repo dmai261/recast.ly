f04b539e19d5c543a9cac8c0e38c4ae937ac4b82
dmai261 <dxmai1314@gmail.com>
2018-08-14 17:28:50 -0700

    Passed the tests for searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,31 +1,31 @@
 var searchYouTube = (options, callback) => {
-  // TODO
-//   var results = YouTube.search.list('id, snippet', {q:'dogs', maxResults: 25});
-// console.log(results);
+  var results;
   $.ajax( {
     url: 'https://www.googleapis.com/youtube/v3/search',
     type: 'GET',
-    
     data: {
+      part: 'snippet',
       q: options.query, 
       maxResults: options.max,  
-      key: options.key, 
-      part: 'snippet'
+      key: options.key,
+      videoEmbaddable: 'true',
+      type: 'video'
     },
-    
-    videoEmbeddable: true,
     success: function(data) {
+      console.log(data);
       console.log('SUCCESS');
-      callback(data);
-      
+      callback(data.items);
+      results = data.items;
     },
     error: function (data) {
       console.log('error');
-      
     }
   });
+  return results;
+  console.log(results);
 };
 
 window.searchYouTube = searchYouTube;
+//console.log(window.searchYouTube);
 
 //key = AIzaSyDr1Di2OLmtk8vtWUMQkxba97UrFpraHcA
